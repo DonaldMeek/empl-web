5c5a2e1c732b37cbd0f9504ff897663505add3fb
root <root@host1.domainUser1.net>
2019-04-30 10:08:09 -0500

    Allow extensible common code by moving classes to rs-common

--- src/main/java/com/meek/donald/EmplWebApplication.java
+++ src/main/java/com/meek/donald/EmplWebApplication.java
@@ -1,12 +1,15 @@
+
 package com.meek.donald;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class EmplWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		public static void main(String[] args) {
+					SpringApplication.run(EmplWebApplication.class, args);
+						}
 }
+
