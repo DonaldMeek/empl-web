874b81320f0acdaf246098cef38215bc88e2c4ce
root <root@host1.domainUser1.net>
2019-04-30 10:23:48 -0500

    Resolved merge conflicts

--- src.main.java/com/meek/donald/employee/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.meek.donald.employee;
-
-public interface EmployeeService{
-	public void insertEmployee(EmployeeModel employeeModel);
-	public EmployeeModel getEmployeeByExample(EmployeeModel employeeModel);
-	public EmployeeModel getEmployeeById(String emplId);
-}
